c879f3d7bd185a0588e0b03d4243bc72b9260616
Anastasia Kutina <kutina223311@gmail.com>
2022-04-02 21:09:07 +0300

    начало работы

---
+++ script.js
@@ -0,0 +1,22 @@
+// var arr = [1, 2, 3, 0, 4, 5, 6];
+
+// let sumi = false;
+// let index = 0;
+
+// let b = arr.reduceRight((prev, item) => {
+//   if (!sumi && prev > 10) {
+//     sumi = index;
+//   }
+//   index++;
+//   return (prev += item);
+// }, 0);
+// console.log(sumi);
+
+let arr = [5, 6, 7, 8, 9];
+
+let arr2=[]
+
+arr.forEach(element => {
+arr2.push(element ** 2)
+});
+console.log(arr2);
